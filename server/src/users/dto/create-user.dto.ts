7fa6766ad14de4f413fc48dd22b376424daf299f
Emmanuel Canqui Pinto <emmanuel.marthin.c@gmail.com>
2025-11-12 19:19:12 -0400

    feat: describe properties in dtos

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,24 +1,54 @@
 import { IsString, IsNotEmpty, IsEnum, IsOptional, MinLength } from 'class-validator';
 import { Role } from '../../common/enums';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({
+    example:'John12',
+    nullable: false,
+    type:'string'
+  })
   @IsString()
   @IsNotEmpty()
   username: string;
-
+  
+  
+  @ApiProperty({
+    example:'JohnDOE1234',
+    nullable: false,
+    minLength: 6
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(6)
   password: string;
-
+  
+  @ApiPropertyOptional({
+    example:'John',
+    nullable: true,
+    type:'string',
+    required:false
+  })
   @IsString()
   @IsOptional()
   name?: string;
-
+  
+  @ApiPropertyOptional({
+    example:'Doe',
+    nullable: true,
+    type:'string',
+    required:false
+  })
   @IsString()
   @IsOptional()
   lastname?: string;
-
+  
+  // @ApiPropertyOptional({
+  @ApiProperty({
+    example:'administrator',
+    nullable: true,
+    enum: Role
+  })
   @IsEnum(Role)
   @IsOptional()
   role?: Role;
