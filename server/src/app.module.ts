ada598c2a1b6296e87f949fc4278c7ecf42cb473
Roman Emiliano Arenas <98709881+RomanArenas32@users.noreply.github.com>
2025-11-14 21:11:16 -0300

    fix-config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -20,6 +20,7 @@ import { AuthGuard } from './common/guards/auth.guard';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
+      url: process.env.DATABASE_URL,
       host: process.env.DATABASE_HOST || 'localhost',
       port: parseInt(process.env.DATABASE_PORT || '5432'),
       username: process.env.DATABASE_USER || 'postgres',
@@ -27,6 +28,7 @@ import { AuthGuard } from './common/guards/auth.guard';
       database: process.env.DATABASE_NAME || 'cms_db',
       entities: [User, Testimonial, Tag, Category],
       synchronize: true,
+      ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
     }),
     UsersModule,
     TestimonialsModule,
