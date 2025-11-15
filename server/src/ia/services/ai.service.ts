c453c61e995e0e00ce5020e87de52790c8a7da94
Keinlinks <kevin.clift.buzeta@gmail.com>
2025-11-15 16:43:07 -0300

    implemented create testimonial pipeline service. To do: implements mediaStorage and aiService

--- server/src/ia/services/ai.service.ts
+++ server/src/ia/services/ai.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import type { IAiProvider } from '../ports/IaiProvider';
+import { Testimonial } from 'src/testimonials/entities/testimonial.entity';
 
 @Injectable()
 export class AiService {
   constructor(private readonly aiProvider: IAiProvider) {}
 
   async evaluate(input: any, options?: Record<string, any>): Promise<any> {
+    return this.aiProvider.evaluate(input, options);
+  }
+  async evaluateTestimonial(testimonial: Testimonial): Promise<string> {
+    //TODO: Implement this.
+    const input = testimonial.content;
+    const options = {};
     return this.aiProvider.evaluate(input, options);
   }
 }
--- server/src/testimonials/services/createTestimonial.service.ts
+++ server/src/testimonials/services/createTestimonial.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Testimonial } from '../entities/testimonial.entity';
 import { MediaStorageService } from 'src/media-storage/services/mediaStorage.service';
 import { CreateTestimonialDto } from '../dto/create-testimonial.dto';
+import Stream from 'stream';
+import { AiService } from 'src/ia/services/ai.service';
 
 @Injectable()
 export class CreateTestimonialsService {
@@ -11,7 +13,57 @@ export class CreateTestimonialsService {
     @InjectRepository(Testimonial)
     private testimonialsRepository: Repository<Testimonial>,
     private readonly mediaStorageService: MediaStorageService,
+    private readonly aiService: AiService,
   ) {}
 
-  excecute(createTestimonialDto: CreateTestimonialDto) {}
+  async createTestimonialWithMedia(
+    createTestimonialDto: CreateTestimonialDto,
+    fileStream: Stream.Readable,
+    filename: string,
+  ): Promise<Testimonial> {
+    const testimonial =
+      this.testimonialsRepository.create(createTestimonialDto);
+    testimonial.status =
+      await this.evaluateStatusTestimonialContent(testimonial);
+    try {
+      let objectFilename = this.generateMediaFilename(
+        createTestimonialDto.user_id,
+        filename,
+      );
+      let publicId = await this.mediaStorageService.uploadFile(
+        fileStream,
+        objectFilename,
+      );
+      testimonial.media_url = publicId;
+      return this.testimonialsRepository.save(testimonial);
+    } catch (error) {
+      throw new InternalServerErrorException(
+        `Failed to upload media: ${error.message}`,
+      );
+    }
+  }
+  async createTestimonial(createTestimonialDto: CreateTestimonialDto) {
+    const testimonial =
+      this.testimonialsRepository.create(createTestimonialDto);
+    testimonial.status =
+      await this.evaluateStatusTestimonialContent(testimonial);
+
+    return this.testimonialsRepository.save(testimonial);
+  }
+
+  private generateMediaFilename(
+    userId: string,
+    originalFilename: string,
+  ): string {
+    const timestamp = Date.now();
+    const sanitizedFilename = originalFilename.replace(/\s+/g, '_');
+    return `testimonials/${userId}/${timestamp}_${sanitizedFilename}`;
+  }
+  private async evaluateStatusTestimonialContent(
+    testimonial: Testimonial,
+  ): Promise<string> {
+    let aiEvaluationResult =
+      await this.aiService.evaluateTestimonial(testimonial);
+    return aiEvaluationResult;
+  }
 }
