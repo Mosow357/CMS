82c2d91a3fe252e2758b5d66e95d83cba3ce1939
Keinlinks <kevin.clift.buzeta@gmail.com>
2025-11-15 16:02:35 -0300

    created ports for ai and media storage features

--- server/src/testimonials/services/createTestimonial.service.ts
+++ server/src/testimonials/services/createTestimonial.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Testimonial } from '../entities/testimonial.entity';
+import { MediaStorageService } from 'src/media-storage/services/mediaStorage.service';
+import { CreateTestimonialDto } from '../dto/create-testimonial.dto';
 
 @Injectable()
 export class CreateTestimonialsService {
   constructor(
     @InjectRepository(Testimonial)
     private testimonialsRepository: Repository<Testimonial>,
+    private readonly mediaStorageService: MediaStorageService,
   ) {}
 
-  excecute() {}
+  excecute(createTestimonialDto: CreateTestimonialDto) {}
 }
