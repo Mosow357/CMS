c86e33da721d0580bd88c5a8fac3b53b9d3d717a
Keinlinks <kevin.clift.buzeta@gmail.com>
2025-11-27 14:25:47 -0300

    refactoring join to orgnization logic

--- server/src/organizations/organitations.module.ts
+++ server/src/organizations/organitations.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Organization } from './entities/organization.entity';
 import { organizationsController } from './controllers/organizations.controller';
-import { OrganizationsService } from './services/organitations.service';
+import { OrganizationsService } from './services/organizations.service';
 import { UserOrganization } from 'src/user_organization/entities/userOrganization.entity';
 
 @Module({
---
+++ server/src/user_organization/services/userOrganization.service.ts
@@ -0,0 +1,22 @@
+import { InjectRepository } from "@nestjs/typeorm";
+import { UserOrganization } from "../entities/userOrganization.entity";
+import { Repository } from "typeorm";
+
+
+export class UserOrganizationService{
+    constructor(@InjectRepository(UserOrganization)
+        private userOrganizationRepository: Repository<UserOrganization>){}
+    async findUserOrganization(userId: string, organizationId: string): Promise<UserOrganization | null> {
+        return this.userOrganizationRepository.findOne({
+            where: {
+                userId,
+                organizationId
+            }
+        });
+    }
+
+    async create(userOrganization: Partial<UserOrganization>): Promise<UserOrganization> {
+        const toSave = this.userOrganizationRepository.create(userOrganization);
+        return this.userOrganizationRepository.save(toSave);
+    }
+}
